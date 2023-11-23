3d00f17bec2929d3402db379be3a351e08fc4c2b
Alejandro Fortes Hidalgo <afortes@eunoia.es>
2023-11-23 10:49:56 +0100

    Skills section

--- components/projects.tsx
+++ components/projects.tsx
@@ -9,7 +9,7 @@ export default function Projects() {
   const { ref } = useSectionInView('Projects', 0.5)
 
   return (
-    <section ref={ref} className=' scroll-mt-28' id='projects'>
+    <section ref={ref} className=' scroll-mt-28 mb-28' id='projects'>
       <SectionHeading>My Projects</SectionHeading>
       {projectsData.map((project, index) => (
         <React.Fragment key={index}>

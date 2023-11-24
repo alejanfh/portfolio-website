8b1b5b88044165fe1290e7343e970b80b6de674b
Alejandro Fortes Hidalgo <afortes@eunoia.es>
2023-11-24 10:01:46 +0100

    Footer section

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import Header from '@/components/header'
 import ActiveSectionContextProvider from '@/context/active-section-context'
 import { Toaster } from 'sonner'
+import Footer from '@/components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,6 +30,7 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+          <Footer />
 
           <Toaster richColors position='top-right' />
         </ActiveSectionContextProvider>

6700e2081a14523736d0627be419ed6b6ee726ad
Alejandro Fortes Hidalgo <afortes@eunoia.es>
2023-11-24 14:49:55 +0100

    Add projects

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { FaReact } from "react-icons/fa";
 import { SiSeat } from "react-icons/si";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import seatWebappImage from "@/public/seat-webapp-image.jpeg";
+import exclusiverseImage from "@/public/exclusiverse-image.png";
+import myPortfolioImage from "@/public/my-portfolio-image.png";
 
 export const links = [
   {
@@ -61,26 +61,27 @@ export const experiencesData = [
 ] as const;
 
 export const projectsData = [
+
   {
-    title: "Elecsum",
+    title: "SEAT Cupra WebApp",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "I worked developing and implementig a well-known car-based Webapp. You can see and interact with it in every SEAT Cupra.",
+    tags: ["Angular", "HTML", "CSS", "Typescript"],
+    imageUrl: seatWebappImage,
   },
   {
-    title: "SEAT Cupra",
+    title: "My portfolio",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      "A fresh React portfolio Website.",
+    tags: ["React", "Next.js", "Frame Motion", "Tailwind", "Typescript CSS", "Email"],
+    imageUrl: myPortfolioImage,
   },
   {
-    title: "Word Analytics",
+    title: "Exclusiverse",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "I worked as a Front-end developer creating this landing page for Exclusiverse, the first NFT Travelers Club with unique benefits.",
+    tags: ["React", "Next.js", "Typescript", "Tailwind"],
+    imageUrl: exclusiverseImage,
   },
 ] as const;
 

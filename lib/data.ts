b5e349f61f9506e298fb36d4b083dc2b34904e9b
Alejandro Fortes Hidalgo <afortes@eunoia.es>
2023-11-24 13:25:50 +0100

    Add experience

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,6 @@
 import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
+import { SiSeat } from "react-icons/si";
 import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
@@ -35,28 +35,28 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Computer Science",
+    location: "FIB - UPC, Barcelona",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "I graduated after 4 years of studying. I immediately found a job as a front-end webapp developer.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2021",
+  },
+  {
+    title: "Front-End WebApp Developer",
+    location: "Movetia - Barcelona",
+    description:
+      "I worked as a front-end webapp developer for 1 year. During this period, I was engaged as a front-end web app developer, contributing to the development and implementation of car-based web applications using Angular for SEAT S.A.",
+    icon: React.createElement(SiSeat),
+    date: "2021 - 2022",
   },
   {
     title: "Front-End Developer",
-    location: "Orlando, FL",
+    location: "Eunoia Digital - Barcelona",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "I'm now a front-end developer currently working in Eunoia Digital, responsible for designing and implementing engaging user interfaces and functionalities using a stack that includes React, Next.js, TypeScript, Tailwind and CSS.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2022 - present",
   },
 ] as const;
 

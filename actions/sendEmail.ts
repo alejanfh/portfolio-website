5db2b6634b05d2152b310b70fc9d465eb920cca5
Alejandro <alejanfh@gmail.com>
2025-10-03 12:53:37 +0200

    fix issue with email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,48 +1,48 @@
-'use server'
+"use server";
 
-import { getErrorMessage, validateString } from '@/lib/utils'
-import { Resend } from 'resend'
-import ContactFormEmail from '@/email/contact-form-email'
-import React from 'react'
+import { getErrorMessage, validateString } from "@/lib/utils";
+import { Resend } from "resend";
+import ContactFormEmail from "@/email/contact-form-email";
+import React from "react";
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
-  const senderEmail = formData.get('senderEmail')
-  const message = formData.get('message')
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
 
   // simple server-side validation
   if (!validateString(senderEmail, 500)) {
     return {
-      error: 'Invalid sender email',
-    }
+      error: "Invalid sender email",
+    };
   }
   if (!validateString(message, 5000)) {
     return {
-      error: 'Invalid message',
-    }
+      error: "Invalid message",
+    };
   }
 
-  let data
+  let data;
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
       to: "alejanfh@gmail.com",
       subject: "Message from contact form",
-      reply_to: senderEmail,
+      replyTo: senderEmail,
       // react.email
       react: React.createElement(ContactFormEmail, {
         message: message,
         senderEmail: senderEmail,
       }),
-    })
+    });
   } catch (error: unknown) {
     return {
       error: getErrorMessage(error),
-    }
+    };
   }
 
   return {
     data,
-  }
-}
+  };
+};
